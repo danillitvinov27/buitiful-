55b7d79f7b674a0048a31baec7e3ce88d1e70cc0
Danil <danil112011@mail.ru>
2022-01-07 23:52:28 +0300

    Initial Commit

---
+++ buitiful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buitiful
+//
+//  Created by Daniil Litvinov on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
